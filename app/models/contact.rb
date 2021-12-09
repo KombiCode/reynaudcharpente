9caa018ba254375b9632fd6da14c0341ce7368ba
Thierry Jet <thierry.jet@kombicode.com>
2021-12-09 14:16:47 +0100

    Add has_many to Contact, validations and nested attribute stuff.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,10 @@
 class Contact < ApplicationRecord
+  has_many :messages, dependent: :destroy
+
+  validates :name, presence: true
+  validates :email, presence: true
+
+  validates :messages, presence: true
+
+  accepts_nested_attributes_for :messages, reject_if: proc { |attributes| attributes['body'].blank? }
 end
