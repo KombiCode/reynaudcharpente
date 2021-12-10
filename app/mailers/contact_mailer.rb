0c975e4b8d5bc572cee205ab689c4a4accf0810b
Thierry Jet <thierry.jet@kombicode.com>
2021-12-10 20:09:24 +0100

    Modify mailer sender

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,6 +2,6 @@ class ContactMailer < ApplicationMailer
   def contact_message_email
     blacklist = ENV["PROHIBITED_EMAILS"]
     @contact = params[:contact]
-    mail(to: 'thierry.jet@kombicode.com', subject: 'New contact message') unless blacklist.include? @contact.email
+    mail(to: 'contact@kombicode.com', subject: 'Nouveau message de contact') unless blacklist.include? @contact.email
   end
 end
