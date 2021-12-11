ca8049acbecdbed2fd9bed1bb6af34acacd529e4
Thierry Jet <thierry.jet@kombicode.com>
2021-12-11 07:52:29 +0100

    Modify path for logo to insert into email.

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,7 @@
 class ContactMailer < ApplicationMailer
   def contact_message_email
     blacklist = ENV["PROHIBITED_EMAILS"]
-    attachments.inline['rc-banner01.png'] = File.read('rc-banner01.png')
+    attachments.inline['rc-banner01.png'] = File.read('app/assets/images/rc-banner01.png')
     @contact = params[:contact]
     mail(to: ENV["DEFAULT_SENDER"], subject: 'Nouveau message de contact') unless blacklist&.include? @contact.email
   end
