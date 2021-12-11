2674fa0372a6632c155d5360c6d0a74e3878e8cf
Thierry Jet <thierry.jet@kombicode.com>
2021-12-11 07:44:37 +0100

    Use translations in message. Try to add logo

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,8 @@
 class ContactMailer < ApplicationMailer
   def contact_message_email
     blacklist = ENV["PROHIBITED_EMAILS"]
+    attachments.inline['rc-banner01.png'] = File.read('rc-banner01.png')
     @contact = params[:contact]
-    mail(to: ENV["DEFAULT_SENDER"], subject: 'Nouveau message de contact') unless blacklist.include? @contact.email
+    mail(to: ENV["DEFAULT_SENDER"], subject: 'Nouveau message de contact') unless blacklist&.include? @contact.email
   end
 end
