62449dd54c1272bfb07aa988207dd635a4827d8f
Thierry Jet <thierry.jet@kombicode.com>
2022-01-19 12:01:56 +0100

    Implement basic management and styling for breadcrumbs

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,5 +1,14 @@
 class EmployeesController < ApplicationController
+  before_action :set_breadcrumbs
+
   def index
     @employees = Employee.all
   end
+
+  private
+
+  def set_breadcrumbs
+    add_breadcrumb("L'équipe", employees_path)
+  end
+
 end
