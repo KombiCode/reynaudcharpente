02856b21c97470e04c29e2b18ac6199271f16c1f
Thierry Jet <thierry.jet@kombicode.com>
2022-01-19 08:54:47 +0100

    Add employees creation to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cleaning database"
+Message.destroy_all
+Contact.destroy_all
+Employee.destroy_all
+
+puts "Add employees"
+Employee.create(
+  name: "Jean-François",
+  job_title: "Gérant",
+  about: "Lauréat du prix BELIN en 1982, CNDB :Maison Bois Outil Concept, Conseiller Européen en Maisons Passives (Certific
+  ation Européenne de Passiv Haus : La Maison Passive)"
+)
+Employee.create(
+  name: "Evelyne",
+  job_title: "Assistante de direction",
+  about: "DUT animation socio-culturel"
+)
+Employee.create(
+  name: "Hugo",
+  job_title: "Ingénieur",
+  about: "CAP de charpente, BTS SCBH (Systèmes Constructifs Bois et Habitat), Ingénieur Génie Civil bois"
+)
+Employee.create(
+  name: "Vincent",
+  job_title: "Technicien",
+  about: "CAP de charpente, BTS SCBH (Systèmes Constructifs Bois et Habitat), Licence professionnelle Bâtiments Bois Basse Co
+nsommation et Passifs 2018-2019, Licence professionnelle métiers de l’entreprenariat parcours TPE"
+)
+Employee.create(
+  name: "Sébastien",
+  job_title: "Opérateur centre d'usinage à commande numérique",
+  about: "BTS SCBH (Systèmes Constructifs Bois et Habitat)"
+)
+puts "#{Employee.count} employees have been created"
+puts "Database ready"
