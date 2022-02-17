b113f862a1bbc2e65015a4dba3f5b3d01f9150cd
Thierry Jet <thierry.jet@kombicode.com>
2022-02-17 13:58:27 +0100

    Prepare stuff for About page. Also fix the footer display/layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "static_pages#home"
-  get 'history', to: 'static_pages#company_history', as: 'history'
-  get 'kc-rc-yec-hed-mat', to: 'static_pages#dashboard', as: 'kc-rc-yec-hed-mat'
+  # get 'history', to: 'static_pages#company_history', as: 'history'
+  # get 'kc-rc-yec-hed-mat', to: 'static_pages#dashboard', as: 'kc-rc-yec-hed-mat'
 
   resources :contacts, only: [ :new, :create, :index ]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
@@ -10,13 +10,19 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   resources :employees
-  # , only: [ :index, :show, :edit, :update, :destroy ]
   resources :skills
-  # , only: [ :index, :new, :create ]
 
   resources :zipcodes, only: :index
 
   resources :activities, only: [ :show ]
+
+  scope controller: :static_pages do
+    get :about
+    # get :terms
+    # get :privacy
+    get :history, to: 'static_pages#company_history', as: 'history'
+    get 'kc-rc-yec-hed-mat', to: 'static_pages#dashboard', as: 'kc-rc-yec-hed-mat'
+    end
 
   # sorcery
   get "logout" => "sessions#destroy", :as => "logout"
