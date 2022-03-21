a5971c78a8ef143cb97fe1e457b317be6adc1fd1
Thierry Jet <thierry.jet@kombicode.com>
2022-03-21 23:13:05 +0100

    Implement display for opinion statistics

--- app/controllers/opinions_controller.rb
+++ app/controllers/opinions_controller.rb
@@ -1,5 +1,24 @@
 class OpinionsController < ApplicationController
   def index
     @opinions = Opinion.all.order("published_at DESC")
+    @op_statistics = statistics
+  end
+
+  private
+  def statistics
+    sum = 0
+    items_count = 0
+    neutral_count = 0
+    bad_count = 0
+    Opinion.all.find_each do |op|
+      sum += op.rating_average
+      items_count += 1
+      if op.bad_rating
+        bad_count += 1
+      elsif op.neutral_rating
+        neutral_count += 1
+      end
+    end
+    OpinionsStatistics.new((sum / items_count).round(1), items_count, neutral_count, bad_count)
   end
 end
--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -2,6 +2,16 @@ class Opinion < ApplicationRecord
   has_many :ratings
   has_many :categories, through: :ratings
 
+  def smiley_for_rating
+    smiley_svg_filename = "smiley-positive.svg"
+    if bad_rating
+      smiley_svg_filename = "smiley-negative.svg"
+    elsif neutral_rating
+      smiley_svg_filename = "smiley-neutral.svg"
+    end
+    smiley_svg_filename
+  end
+
   def rating_average
     sum = 0
     ratings.each do |rating|
@@ -9,4 +19,16 @@ class Opinion < ApplicationRecord
     end
     (sum / ratings.count).round(1)
   end
+
+  def bad_rating
+    rating_average < 5.1
+  end
+  def neutral_rating
+    ra = rating_average
+    ra > 5.1 && ra < 7.5
+  end
+  def good_rating
+    rating_average >= 7.5
+  end
+
 end
